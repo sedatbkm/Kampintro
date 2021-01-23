1aa79b328cd74ff11de359b88d6dfdd7ee57b5a6
Sedat BAKIM <sedatbkm@gmail.com>
2021-01-23 23:51:35 +0300

    23 Ocak Ders Sonu

---
+++ Oop2/MusteriMenager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    //iş sınıfları - business classes
+    class MusteriMenager
+    {
+        public void Add(Musteri musteri) 
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
